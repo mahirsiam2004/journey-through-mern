266b4cebb9500af9d2fd4cccb0ed65e6207bc760
mahirsiam2004 <mahirsiam2004@gmail.com>
2025-07-26 22:03:59 +0600

    added js in mern

---
+++ Javascript/playwithvar.js
@@ -0,0 +1,23 @@
+var orange = 20;
+var coco = 0.5;
+var apple = "20";
+var ans=parseInt('augdajdsuab');
+
+// console.log(typeof coco);
+console.log(typeof ans);
+console.log(ans+coco);
+
+
+var first=0.1;
+var second=0.2;
+var total=first+second;
+// console.log(total);
+console.log(total.toFixed(2));
+
+const num=-50/0;
+console.log(num);
+
+console.log(40+"40");
+console.log("40"-10);
+console.log(40-"10");
+console.log("40"+10);
