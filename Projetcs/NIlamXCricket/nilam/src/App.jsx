f41cf7f488b71debba5a7d7216f531ac30fbb0ac
mahirsiam2004 <mahirsiam2004@gmail.com>
2025-09-23 23:41:07 +0600

    added project with api in react

--- Projetcs/NIlamXCricket/nilam/src/App.jsx
+++ Projetcs/NIlamXCricket/nilam/src/App.jsx
@@ -3,17 +3,20 @@ import navPng from "./assets/logo.png";
 function App() {
   return (
     <>
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar max-w-[1200px] mx-auto">
         <div className="flex-1">
           <a className="btn btn-ghost text-xl">
             <img className="w-[60px] h-[60px]" src={navPng} alt="" />
           </a>
         </div>
         <div className="flex-none">
-    <span>60000000</span>
-    <span>coin</span>
+          <span className="mr-1">60000000</span>
+          <span>coin</span>
         </div>
       </div>
+
+
+
     </>
   );
 }
