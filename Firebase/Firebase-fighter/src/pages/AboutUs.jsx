d78d9196529246b9ce0df706a712dbd8f541d77d
mahirsiam2004 <mahirsiam2004@gmail.com>
2025-10-21 16:59:35 +0600

    added more

--- Firebase/Firebase-fighter/src/pages/AboutUs.jsx
+++ Firebase/Firebase-fighter/src/pages/AboutUs.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
 const AboutUs = () => {
+  const [date, setDate] = useState(new Date());
+
+  console.log(date);
+
   const handleNotify = () => {
     toast.success("🎉 You’ll be notified when the About Us page is ready!", {
       position: "top-center",
@@ -10,11 +14,25 @@ const AboutUs = () => {
     });
   };
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-br from-purple-300 via-purple-500 to-purple-300 text-center">
       <h1 className="text-5xl md:text-6xl font-bold text-white animate-bounce drop-shadow-lg">
         🚀 About Us page is Coming Soon
       </h1>
+
+      <h2 className="font-bold text-white text-3xl my-5">
+        {date.toLocaleTimeString()}
+      </h2>
 
       <p className="mt-5 text-lg text-white/90 animate-pulse">
         Stay tuned! We’re crafting something amazing for you ✨
--- Firebase/Firebase-fighter/src/pages/Signup.jsx
+++ Firebase/Firebase-fighter/src/pages/Signup.jsx
@@ -1,24 +1,46 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 import { FaEye } from "react-icons/fa";
 
 import { IoEyeOff } from "react-icons/io5";
 
 import MyContainer from "../components/MyContainer";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  updateProfile,
+} from "firebase/auth";
 import { auth } from "../firebase/firebase.config";
 import { toast } from "react-toastify";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const Signup = () => {
   const [show, setShow] = useState(false);
+  const {
+    createUserWithEmailAndPasswordFunc,
+    updateProfileFunc,
+    sendEmailVerificationFunc,
+    setLoading,
+    signoutUserFunc,
+    setUser,
+  } = useContext(AuthContext);
+  const navigate = useNavigate();
 
   const handleSignup = (e) => {
     e.preventDefault();
+    const displayName = e.target.name?.value;
+    const photoURL = e.target.photo?.value;
     const email = e.target.email?.value;
     const password = e.target.password?.value;
 
-    console.log("signup function entered", { email, password });
+    console.log("signup function entered", {
+      email,
+      displayName,
+      photoURL,
+      password,
+    });
+
     // console.log(password.length);
     // if (password.length < 6) {
     //   toast.error("Password should be at least 6 digit");
@@ -37,10 +59,38 @@ const Signup = () => {
       return;
     }
 
-    createUserWithEmailAndPassword(auth, email, password)
+    // 1st step : Create user
+    // createUserWithEmailAndPassword(auth, email, password);
+    createUserWithEmailAndPasswordFunc(email, password)
       .then((res) => {
-        console.log(res);
-        toast.success("Signup successful");
+        // 2nd step: Update profile
+        updateProfileFunc(displayName, photoURL)
+          .then(() => {
+            console.log(res);
+            // 3rd step: Email verification
+            sendEmailVerificationFunc()
+              .then((res) => {
+                console.log(res);
+                setLoading(false);
+
+                // Signout user
+                signoutUserFunc().then(() => {
+                  toast.success(
+                    "Signup successful. Check your email to validate your account. "
+                  );
+                  setUser(null);
+                  navigate("/signin");
+                });
+              })
+              .catch((e) => {
+                console.log(e);
+                toast.error(e.message);
+              });
+          })
+          .catch((e) => {
+            console.log(e);
+            toast.error(e.message);
+          });
       })
       .catch((e) => {
         console.log(e);
@@ -97,6 +147,25 @@ const Signup = () => {
             </h2>
 
             <form onSubmit={handleSignup} className="space-y-4">
+              <div>
+                <label className="block text-sm font-medium mb-1">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Habib utsho"
+                  className="input input-bordered w-full bg-white/20 text-white placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-pink-400"
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium mb-1">Photo</label>
+                <input
+                  type="text"
+                  name="photo"
+                  placeholder="Your photo URL here"
+                  className="input input-bordered w-full bg-white/20 text-white placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-pink-400"
+                />
+              </div>
+
               <div>
                 <label className="block text-sm font-medium mb-1">Email</label>
                 <input
