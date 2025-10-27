7f3243d52142088e3b9a98e1d721c8f0ef3ff2ee
mahirsiam2004 <mahirsiam2004@gmail.com>
2025-10-27 18:47:10 +0600

    added express noob server

---
+++ Express/my-first-server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("hello world ");
+});
+
+app.get("/data", (req, res) => {
+  res.send("more data coming soon");
+});
+
+app.listen(port, () => {
+  console.log("my first server");
+});
