ee2ad44796324a2f2c451a7e621812758e4e8f10
mahirsiam2004 <mahirsiam2004@gmail.com>
2026-07-26 00:05:58 +0600

    added std express servera

---
+++ Express X Mongodb/STD_recape/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+require('dotenv').config()
+const app = express();
+const connectDB = require('./src/config/db')
+
+
+
+
+connectDB();
+
+
+
+
+app.listen(3000,()=>{
+    console.log("koapa--------chamcu")
+})
